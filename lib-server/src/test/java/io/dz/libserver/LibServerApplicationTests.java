8c876559653c5b137f3d435fe0976b64ea3a6300
Purushottam <purushottamn8@gmail.com>
2020-05-09 12:42:00 +0530

    library server service

---
+++ lib-server/src/test/java/io/dz/libserver/LibServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dz.libserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
